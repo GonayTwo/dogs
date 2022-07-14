c496bb2dff6c267cd45f1a072d066404ee365609
Daniel Miranda <daniel_miranda97@hotmail.com>
2022-07-13 21:37:18 -0300

    Criação do Hook de formulário -  useForm

--- src/Components/Forms/Input.jsx
+++ src/Components/Forms/Input.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './Input.module.css';
 
 const Input = (props) => {
+  console.log(props);
   return (
     <div className={styles.wrapper}>
       <label htmlFor={props.name} className={styles.label}>
@@ -13,8 +14,11 @@ const Input = (props) => {
         name={props.name}
         className={styles.input}
         type={props.type}
+        value={props.value}
+        onChange={props.onChange}
+        onBlur={props.onBlur}
       />
-      <p className={styles.error}>Error</p>
+      {props.error && <p className={styles.error}>{props.error}</p>}
     </div>
   );
 };
