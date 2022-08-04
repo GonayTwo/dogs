11a8562462dd454904fa7bf1e3396699c8d88c09
Daniel Miranda <daniel_miranda97@hotmail.com>
2022-08-04 11:48:44 -0300

    Criação de erro 404 para rota não encontrada

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import User from './Components/User/User';
 import { UserStorage } from './UserContext';
 import Photo from './Components/Photo/Photo';
 import UserProfile from './Components/User/UserProfile';
+import NotFound from './Components/NotFound';
 function App() {
   return (
     <div className='App'>
@@ -28,6 +29,7 @@ function App() {
             />
             <Route path='foto/:id' element={<Photo />} />
             <Route path='perfil/:user' element={<UserProfile />} />
+            <Route path='*' element={<NotFound />} />
           </Routes>
           <Footer />
         </UserStorage>
