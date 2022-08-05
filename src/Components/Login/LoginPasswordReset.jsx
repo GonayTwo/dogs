0e203eddb1f0f311a2688bbaba3fcc5f39f8d5e0
Daniel Miranda <daniel_miranda97@hotmail.com>
2022-08-05 16:55:02 -0300

    Ajustes finais

--- src/Components/Login/LoginPasswordReset.jsx
+++ src/Components/Login/LoginPasswordReset.jsx
@@ -36,7 +36,7 @@ const LoginPasswordReset = () => {
     }
   }
   return (
-    <div>
+    <section className='animeLeft'>
       <Head title='Resete a senha' />
       <h1 className='title'>Resete a senha</h1>
       <form onSubmit={handleSubmit}>
@@ -49,7 +49,7 @@ const LoginPasswordReset = () => {
         {loading ? <Button>Resetando...</Button> : <Button>Resetar</Button>}
       </form>
       <Error error={error} />
-    </div>
+    </section>
   );
 };
 
